4c339e803ee744f4b58df990c5d2f564da177eee
HappyMan <100917359+NPChappyman@users.noreply.github.com>
2024-10-27 01:13:33 +0300

    mmm

--- array/LowArrayApp.java
+++ array/LowArrayApp.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class LowArrayApp {
 	
 	 public static void main(String[] args)
-	    {
+	    {  
 		 w.ww.test();
 		int[] a = new int[] {12,313,141,1,42,2,4144,17};
 		sort.bubblesort(a);
@@ -14,25 +14,19 @@ public class LowArrayApp {
 			System.out.print(a[i]+" ");
 		}
 		System.out.println();
-			  
-		System.out.println(reverseWords("S[alam] aleykum world!"));
-		
+		stack.stack thestack = new stack.stack(7);
+		thestack.push(7);
+		thestack.push(11);
+		thestack.push(18);
+		thestack.push(90);
+		thestack.push(13);
+		while (!thestack.isEmpty())
+		{
+			System.out.print(thestack.pop());
+			System.out.print(" ");
+			
 		}
-	 
-	 public static String reverseWords(final String original)
-	  {
-	      String answer = "";
-	    String placeholder = "";
-	    for(int i = original.length() - 1; i >= 0; i-- ){
-	      if(original.charAt(i) != ' '){
-	        placeholder = placeholder + original.charAt(i);
-	      }
-	      else{
-	        answer = " " + placeholder + answer;
-	        placeholder = "";
-	      }
-	    }
-	    answer = placeholder + answer;
-	    return answer;     
+		System.out.println("");
+		
 	  }
 }
